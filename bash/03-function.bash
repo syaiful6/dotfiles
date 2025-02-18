c33488512532aae286fbba2eff55d45f7cec324b
Syaiful Bahri <syai.bahri6@gmail.com>
2025-02-18 23:31:29 +0700

    lazy vim setup for macos

--- bash/03-function.bash
+++ bash/03-function.bash
@@ -1,52 +1,52 @@
 extract() {
-    if [ -f $1 ] ; then
-        case $1 in
-            *.tar.bz2)   tar xvjf $1    ;;
-            *.tar.gz)    tar xvzf $1    ;;
-            *.tar.xz)    tar xvJf $1    ;;
-            *.bz2)       bunzip2 $1     ;;
-            *.rar)       unrar x $1     ;;
-            *.gz)        gunzip $1      ;;
-            *.tar)       tar xvf $1     ;;
-            *.tbz2)      tar xvjf $1    ;;
-            *.tgz)       tar xvzf $1    ;;
-            *.zip)       unzip $1       ;;
-            *.Z)         uncompress $1  ;;
-            *.7z)        7z x $1        ;;
-            *.xz)        unxz $1        ;;
-            *.exe)       cabextract $1  ;;
-            *)           echo "\`$1': unrecognized file compression" ;;
-        esac
-    else
-        echo "\`$1' is not a valid file"
-    fi
+  if [ -f $1 ]; then
+    case $1 in
+    *.tar.bz2) tar xvjf $1 ;;
+    *.tar.gz) tar xvzf $1 ;;
+    *.tar.xz) tar xvJf $1 ;;
+    *.bz2) bunzip2 $1 ;;
+    *.rar) unrar x $1 ;;
+    *.gz) gunzip $1 ;;
+    *.tar) tar xvf $1 ;;
+    *.tbz2) tar xvjf $1 ;;
+    *.tgz) tar xvzf $1 ;;
+    *.zip) unzip $1 ;;
+    *.Z) uncompress $1 ;;
+    *.7z) 7z x $1 ;;
+    *.xz) unxz $1 ;;
+    *.exe) cabextract $1 ;;
+    *) echo "\`$1': unrecognized file compression" ;;
+    esac
+  else
+    echo "\`$1' is not a valid file"
+  fi
 }
 
 # Read text from stdin, and echo a version of the same text, just better suited
 # for a filename.
 sanitise-filename() {
-    echo -n "$1" | tr -c -s 'a-zA-Z0-9.-' '-'
+  echo -n "$1" | tr -c -s 'a-zA-Z0-9.-' '-'
 }
 
 trim-file-prefix() {
-    echo -n "$1" | cut -c18-
+  echo -n "$1" | cut -c18-
 }
 
 trim-files-prefix() {
-    local exitcode=0
-    local sanitised
-    for file in $*; do
-        sanitised=$(trim-file-prefix "$file")
+  local exitcode=0
+  local sanitised
+  for file in $*; do
+    sanitised=$(trim-file-prefix "$file")
 
-        [ "$sanitised" = "$file" ] && continue
+    [ "$sanitised" = "$file" ] && continue
 
-        if [ -f "$sanitised" ]; then
-            echo >&2 "$0: can't rename $file, $sanitised already exists"
-            exitcode=1
-        else
-            mv -- "$file" "$sanitised"
-        fi
-    done
+    if [ -f "$sanitised" ]; then
+      echo >&2 "$0: can't rename $file, $sanitised already exists"
+      exitcode=1
+    else
+      mv -- "$file" "$sanitised"
+    fi
+  done
 }
 
 # Wrapper around sanitise-filename which takes a list of files and renames them
@@ -56,98 +56,121 @@ trim-files-prefix() {
 #   sanitise-rename-files *.doc
 #
 sanitise-rename-files() {
-    local exitcode=0
-    local sanitised
+  local exitcode=0
+  local sanitised
 
-    for file in $*; do
-        sanitised=$(sanitise-filename "$file")
+  for file in $*; do
+    sanitised=$(sanitise-filename "$file")
 
-        [ "$sanitised" = "$file" ] && continue
+    [ "$sanitised" = "$file" ] && continue
 
-        if [ -f "$sanitised" ]; then
-            echo >&2 "$0: can't rename $file, $sanitised already exists"
-            exitcode=1
-        else
-            mv -- "$file" "$sanitised"
-        fi
-    done
+    if [ -f "$sanitised" ]; then
+      echo >&2 "$0: can't rename $file, $sanitised already exists"
+      exitcode=1
+    else
+      mv -- "$file" "$sanitised"
+    fi
+  done
 
-    return $exitcode
+  return $exitcode
 }
 
 dotfile-has() {
-  type "${1-}" > /dev/null 2>&1
+  type "${1-}" >/dev/null 2>&1
 }
 
 # generate random password/key
 gen-pswd() {
-    if dotfile-has "pwgen"; then
-        # we have pwgen use it
-        command pwgen -ysB $1
-    else
-        # use system random
-            </dev/urandom tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' | head -c $1  ; echo
-    fi
+  if dotfile-has "pwgen"; then
+    # we have pwgen use it
+    command pwgen -ysB $1
+  else
+    # use system random
+    </dev/urandom tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' | head -c $1
+    echo
+  fi
 }
 
 # function for adding dirs to $PATH
 # silently does nothing if an argument isn't a directory
 add-path() {
-    local PREPEND="false"
+  local PREPEND="false"
 
-    # basic arg parsing
-    if [ "$1" = "--prepend" ]; then
-        PREPEND="true"
-        shift
+  # basic arg parsing
+  if [ "$1" = "--prepend" ]; then
+    PREPEND="true"
+    shift
+  fi
+
+  for ARG in $*; do
+    if [ -d $ARG ]; then
+      if [ "$PREPEND" = "true" ]; then
+        export PATH="$ARG:$PATH"
+      else
+        export PATH="$PATH:$ARG"
+      fi
     fi
+  done
+}
 
-    for ARG in $*; do
-        if [ -d $ARG ]; then
-            if [ "$PREPEND" = "true" ]; then
-                export PATH="$ARG:$PATH"
-            else
-                export PATH="$PATH:$ARG"
-            fi
-        fi
-    done
+# verify that all text files in a trailing newline
+check-trailing-newline() {
+  while read -r line; do
+    if ! [[ -s "${line}" && -z "$(tail -c 1 "${line}")" ]]; then
+      printf "File must end in a trailing newline: %s\n" "${line}" >&2
+    fi
+  done < <(git ls-files |
+    xargs grep ".*" \
+      --files-with-matches \
+      --binary-files=without-match \
+      --exclude="*third-party*")
+}
+
+check-trailing-whitespace() {
+  while read -r line; do
+    if grep \
+      "\s$" \
+      --line-number \
+      --with-filename \
+      --binary-files=without-match \
+      "${line}"; then
+      echo "ERROR: Found trailing whitespace"
+    fi
+  done < <(git ls-files)
 }
 
 se_download_html() {
-    command curl http://95.217.135.107/media/serp_html/${1}.html -L --output ${1}.html
+  command curl http://95.217.135.107/media/serp_html/${1}.html -L --output ${1}.html
 }
 
 lambda-rust-deploy-all-regions() {
-    declare -a regions=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ap-northeast-2" "ap-northeast-3" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "sa-east-1" "eu-south-1")
-    for i in "${regions[@]}"
-    do
-        echo "deploy to $i"
-        aws lambda create-function --function-name ${1} --runtime provided.al2 --handler hello --role 'arn:aws:iam::749323766402:role/awslambdaproxy-role' --zip-file ${2} --region ${i} --timeout 120 >> /dev/null 2>&1
-    done
+  declare -a regions=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ap-northeast-2" "ap-northeast-3" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "sa-east-1" "eu-south-1")
+  for i in "${regions[@]}"; do
+    echo "deploy to $i"
+    aws lambda create-function --function-name ${1} --runtime provided.al2 --handler hello --role 'arn:aws:iam::749323766402:role/awslambdaproxy-role' --zip-file ${2} --region ${i} --timeout 120 >>/dev/null 2>&1
+  done
 }
 
 lambda-rust-deploy-arm64-all-regions() {
-    declare -a regions=("us-east-1" "us-east-2" "us-west-2" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "eu-central-1" "eu-west-1" "eu-west-2")
-    for i in "${regions[@]}"
-    do
-        echo "deploy to $i"
-        aws lambda create-function --function-name ${1} --runtime provided.al2 --handler hello --architectures arm64 --role 'arn:aws:iam::749323766402:role/awslambdaproxy-role' --zip-file ${2} --region ${i} --timeout 120 >> /dev/null 2>&1
-    done
+  declare -a regions=("us-east-1" "us-east-2" "us-west-2" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "eu-central-1" "eu-west-1" "eu-west-2")
+  for i in "${regions[@]}"; do
+    echo "deploy to $i"
+    aws lambda create-function --function-name ${1} --runtime provided.al2 --handler hello --architectures arm64 --role 'arn:aws:iam::749323766402:role/awslambdaproxy-role' --zip-file ${2} --region ${i} --timeout 120 >>/dev/null 2>&1
+  done
 }
 
 gcloud-go-deploy-tier1() {
-    declare -a regions=("us-west1" "us-central1" "us-east1" "us-east4" "europe-west1" "europe-west2" "asia-east1" "asia-east2" "asia-northeast1" "asia-northeast2")
-    for i in "${regions[@]}"
-    do
-        echo "deploy to $i"
-        gcloud functions deploy ${1} --entry-point=KanakFnHTTP --runtime go116 --trigger-http --allow-unauthenticated --region ${i} >> /dev/null 2>&1
-    done
+  declare -a regions=("us-west1" "us-central1" "us-east1" "us-east4" "europe-west1" "europe-west2" "asia-east1" "asia-east2" "asia-northeast1" "asia-northeast2")
+  for i in "${regions[@]}"; do
+    echo "deploy to $i"
+    gcloud functions deploy ${1} --entry-point=KanakFnHTTP --runtime go116 --trigger-http --allow-unauthenticated --region ${i} >>/dev/null 2>&1
+  done
 }
 
 gcloud-go-deploy-tier2() {
-    declare -a regions=("us-west2" "us-west3" "us-west4" "northamerica-northeast1" "southamerica-east1" "europe-west3" "europe-west6" "europe-central2" "australia-southeast1" "asia-south1" "asia-southeast1" "asia-southeast2" "asia-northeast3")
-    for i in "${regions[@]}"
-    do
-        echo "deploy to $i"
-        gcloud functions deploy ${1} --entry-point=KanakFnHTTP --runtime go116 --trigger-http --allow-unauthenticated --region ${i} >> /dev/null 2>&1
-    done
+  declare -a regions=("us-west2" "us-west3" "us-west4" "northamerica-northeast1" "southamerica-east1" "europe-west3" "europe-west6" "europe-central2" "australia-southeast1" "asia-south1" "asia-southeast1" "asia-southeast2" "asia-northeast3")
+  for i in "${regions[@]}"; do
+    echo "deploy to $i"
+    gcloud functions deploy ${1} --entry-point=KanakFnHTTP --runtime go116 --trigger-http --allow-unauthenticated --region ${i} >>/dev/null 2>&1
+  done
 }
--- config/nvim/lua/plugins/extras/lang/python.lua
+++ config/nvim/lua/plugins/extras/lang/python.lua
@@ -1,69 +1,81 @@
+if lazyvim_docs then
+  -- LSP Server to use for Python.
+  -- Set to "basedpyright" to use basedpyright instead of pyright.
+  vim.g.lazyvim_python_lsp = "pyright"
+  -- Set to "ruff_lsp" to use the old LSP implementation version.
+  vim.g.lazyvim_python_ruff = "ruff"
+end
+
+local lsp = vim.g.lazyvim_python_lsp or "pyright"
+local ruff = vim.g.lazyvim_python_ruff or "ruff"
+
 return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = "python",
+      root = {
+        "pyproject.toml",
+        "setup.py",
+        "setup.cfg",
+        "requirements.txt",
+        "Pipfile",
+        "pyrightconfig.json",
+      },
+    })
+  end,
   {
     "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "ninja", "rst" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruff = {
+          cmd_env = { RUFF_TRACE = "messages" },
+          init_options = {
+            settings = {
+              logLevel = "error",
+            },
+          },
+          keys = {
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
+            },
+          },
+        },
+        ruff_lsp = {
+          keys = {
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
+            },
+          },
+        },
+      },
+      setup = {
+        [ruff] = function()
+          LazyVim.lsp.on_attach(function(client, _)
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+          end, ruff)
+        end,
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "ninja", "python", "rst", "toml" })
+      local servers = { "pyright", "basedpyright", "ruff", "ruff_lsp", ruff, lsp }
+      for _, server in ipairs(servers) do
+        opts.servers[server] = opts.servers[server] or {}
+        opts.servers[server].enabled = server == lsp or server == ruff
       end
     end,
   },
-  -- Add `pyright` to mason
-  -- TODO: check following tools -> mypy types-requests types-docutils
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "black",
-        "ruff",
-      })
-    end,
-  },
-
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "mfussenegger/nvim-dap-python",
-      keys = {
-        {
-          "<leader>dPt",
-          function()
-            require("dap-python").test_method()
-          end,
-          desc = "Debug Method",
-        },
-        {
-          "<leader>dPc",
-          function()
-            require("dap-python").test_class()
-          end,
-          desc = "Debug Class",
-        },
-      },
-      config = function()
-        local path = require("mason-registry").get_package("debugpy"):get_install_path()
-        require("dap-python").setup(path .. "/venv/bin/python")
-      end,
-    },
-  },
-
-  -- Add `python` debugger to mason DAP to auto-install
-  -- Not absolutely necessary to declare adapter in `ensure_installed`, since `mason-nvim-dap`
-  -- has `automatic-install = true` in LazyVim by default and it automatically installs adapters
-  -- that are are set up (via dap) but not yet installed. Might as well skip the lines below as
-  -- a whole.
-
-  -- Add which-key namespace for Python debugging
-  {
-    "folke/which-key.nvim",
-    optional = true,
-    opts = {
-      spec = {
-        ["<leader>dP"] = { name = "+Python" },
-      },
-    },
-  },
-
-  -- Setup `neotest`
   {
     "nvim-neotest/neotest",
     optional = true,
@@ -80,59 +92,62 @@ return {
       },
     },
   },
-
-  -- Add `server` and setup lspconfig
   {
-    "neovim/nvim-lspconfig",
-    dependencies = {},
-    opts = {
-      servers = {
-        -- pyright = {},
-        ruff_lsp = {},
-        jedi_language_server = {},
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      "mfussenegger/nvim-dap-python",
+      -- stylua: ignore
+      keys = {
+        { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+        { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
       },
-      setup = {
-        ruff_lsp = function()
-          require("lazyvim.util").lsp.on_attach(function(client, _)
-            if client.name == "ruff_lsp" then
-              -- Disable hover in favor of Pyright
-              client.server_capabilities.hoverProvider = false
-            end
-          end)
-        end,
-        pyright = function()
-          require("lazyvim.util").lsp.on_attach(function(client, _)
-            if client.name == "pyright" then
-              -- disable hover in favor of jedi-language-server
-              client.server_capabilities.hoverProvider = false
-            end
-          end)
-        end,
-      },
+      config = function()
+        if vim.fn.has("win32") == 1 then
+          require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/Scripts/pythonw.exe"))
+        else
+          require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/bin/python"))
+        end
+      end,
     },
   },
 
   {
-    "nvimtools/none-ls.nvim",
+    "linux-cultist/venv-selector.nvim",
+    branch = "regexp", -- Use this branch for the new version
+    cmd = "VenvSelect",
+    enabled = function()
+      return LazyVim.has("telescope.nvim")
+    end,
+    opts = {
+      settings = {
+        options = {
+          notify_user_on_venv_activation = true,
+        },
+      },
+    },
+    --  Call config for python files and load the cached venv automatically
+    ft = "python",
+    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
     opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = vim.list_extend(opts.sources, {
-        -- Order of formatters matters. They are used in order of appearance.
-        nls.builtins.formatting.ruff,
-        nls.builtins.formatting.black,
-        -- nls.builtins.formatting.black.with({
-        --   extra_args = { "--preview" },
-        -- }),
-        -- nls.builtins.diagnostics.ruff,
-      })
+      opts.auto_brackets = opts.auto_brackets or {}
+      table.insert(opts.auto_brackets, "python")
     end,
   },
 
-  -- For selecting virtual envs
+  -- Don't mess up DAP adapters provided by nvim-dap-python
   {
-    "linux-cultist/venv-selector.nvim",
-    cmd = "VenvSelect",
-    opts = {},
-    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = {
+      handlers = {
+        python = function() end,
+      },
+    },
   },
 }
