ab6f0c070ff1c8d019fc78dd041003ea663af20e
Syaiful Bahri <syai.bahri6@gmail.com>
2025-09-22 23:46:13 +0700

    clean up neovim configuration

--- bash/01-aliases.bash
+++ bash/01-aliases.bash
@@ -1,3 +1,7 @@
 alias docomp='docker run --rm --interactive --tty --volume "$PWD":/app composer'
+alias gp='git push'
+alias gpo='git push origin'
+alias gl='git pull'
+alias glo='git pull origin'
 # alias for tmux
 tmux="TERM=screen-256color-bce tmux"
