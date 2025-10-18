5cba5fcb2f1e5d61ec0e1c7ad28a86bf7bc2f1cc
Syaiful Bahri <syai.bahri6@gmail.com>
2025-10-18 20:08:55 +0700

    Fix blink completion icon

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -29,23 +29,25 @@ return {
         implementation = "prefer_rust_with_warning",
       },
       completion = {
-        draw = {
-          components = {
-            kind_icon = {
-              text = function(ctx)
-                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-                return kind_icon
-              end,
-              highlight = function(ctx)
-                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                return hl
-              end,
-            },
-            kind = {
-              highlight = function(ctx)
-                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                return hl
-              end,
+        menu = {
+          draw = {
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return kind_icon
+                end,
+                highlight = function(ctx)
+                  local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return hl
+                end,
+              },
+              kind = {
+                highlight = function(ctx)
+                  local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return hl
+                end,
+              },
             },
           },
         },
